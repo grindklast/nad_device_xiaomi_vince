d11286678f868c37a0c4f33d2da1ca584ec8f7b9
KenHV <kenhv@null.net>
2020-08-30 18:01:05 +0530

    vince: build dirac
    Signed-off-by: KenHV <kenhv@null.net>

--- device.mk
+++ device.mk
@@ -193,6 +193,10 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI \
     NexusLauncherRelease
 
+# Dirac
+PRODUCT_PACKAGES += \
+    Dirac
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
